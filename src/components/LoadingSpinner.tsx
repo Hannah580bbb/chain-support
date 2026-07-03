aa61bfd8fe3f4e04f14cd22fa07066e9b4f04b3c
Hannah580bbb <hannah.johanson@montaracapitals.com>
2026-07-03 09:17:09 +0100

    Add LoadingSpinner component

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export function LoadingSpinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="animate-spin rounded-full h-8 w-8 border border-blue-500/30 border-t-blue-500"></div>
+    </div>
+  );
+}
